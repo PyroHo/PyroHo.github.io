d2ccd21ee50ed8667d29a5c693646d5f7cbdac82
PyroHo <1084009635@qq.com>
2019-03-29 11:57:30 +0800

    Site updated: 2019-03-29 11:57:29

--- js/canvas.js
+++ js/canvas.js
@@ -1,24 +1,22 @@
-/* >= */
 
-alert(1);
-var canvas = document.querySelector('canvas');
-canvas.height = canvas.width = 256.5;
-var ctx = canvas.getContext('2d');
-ctx.setLineDash([8,4]);
-var offset = 0;
+window.addEventListener('loaded', function() {
+    var canvas = document.querySelector('canvas');
+    canvas.height = canvas.width = 256.5;
+    var ctx = canvas.getContext('2d');
+    ctx.setLineDash([8,4]);
+    var offset = 0;
 
-function draw() {
-    offset++;
-    if( offset >= 12){
-        offset = 0;
+    function draw() {
+        offset++;
+        if( offset >= 12){
+            offset = 0;
+        }
+        ctx.clearRect( 0, 0, canvas.width, canvas.height);
+        ctx.lineDashOffset = -offset;
+        ctx.lineWidth = 3;
+        ctx.strokeRect(50, 50, 200, 200);
+        setTimeout(draw, 16);
     }
-    ctx.clearRect( 0, 0, canvas.width, canvas.height);
-    ctx.lineDashOffset = -offset;
-    ctx.lineWidth = 3;
-    ctx.strokeRect(50, 50, 200, 200);
-    setTimeout(draw, 16);
-}
 
-draw();
-
-alert(2);
+    draw();
+}, false);
